cd1400deacbc286cead29f3887f931485ae9f709
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-03-12 19:48:53 +0530

    videocontroller

--- app/Http/Controllers/API/VideoController.php
+++ app/Http/Controllers/API/VideoController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 class VideoController extends Controller
 {
     public function upload_video(Request $request){
+       
         $validator = \Validator::make($request->all(), [
             'base_table_id'=>'required',
             'standard_id'=> 'required',
@@ -20,11 +21,16 @@ class VideoController extends Controller
             'video_category_id'=>'required',
             'topic_video' => 'required|mimes:mp4,mov,avi|max:10240', // assuming you want to limit to specific video types and a max size of 10MB
         ]);
-
+        
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 400);
         }
-
+       
+        if ($request->hasFile('topic_video')) {
+            $videoPath = $request->file('topic_video')->store('videos', 'public');
+            //$topic->update(['topic_video' => $videoPath]);
+        }
+        
         $topic = Topic_model::create([
             'base_table_id'=>$request->input('base_table_id'),
             'standard_id'=> $request->input('standard_id'),
@@ -33,12 +39,10 @@ class VideoController extends Controller
             'topic_no' => $request->input('topic_no'),
             'topic_name' => $request->input('topic_name'),
             'video_category_id'=>$request->input('video_category_id'),
+            'topic_video'=>$videoPath
         ]);
-
-        if ($request->hasFile('topic_video')) {
-            $videoPath = $request->file('topic_video')->store('videos', 'public');
-            $topic->update(['topic_video' => $videoPath]);
-        }
+        
+        
 
         return response()->json(['message' => 'Topic and video uploaded successfully', 'topic' => $topic]);
   
