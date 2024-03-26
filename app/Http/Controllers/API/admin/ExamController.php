033df85302eded70b1bb237e04f103d4d32ebe68
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-03-26 11:18:29 +0530

    code

--- app/Http/Controllers/API/admin/ExamController.php
+++ app/Http/Controllers/API/admin/ExamController.php
@@ -39,31 +39,45 @@ class ExamController extends Controller
                     'standard_id' => 'required',
                     'subject_id' => 'required',
                 ]);
-
-                $exam = new Exam_Model;
-                $exam->exam_title = $validatedData['exam_title'];
-                $exam->total_mark = $validatedData['total_mark'];
-                $exam->exam_type = $validatedData['exam_type'];
-                $exam->exam_date = Carbon::createFromFormat('d-m-Y', $validatedData['exam_date']);
-                $exam->start_time = $validatedData['start_time'];
-                $exam->end_time = $validatedData['end_time'];
-                $exam->institute_for_id = $validatedData['institute_for_id'];
-                $exam->board_id = $validatedData['board_id'];
-                $exam->medium_id = $validatedData['medium_id'];
-                $exam->class_id = $validatedData['class_id'];
-                $exam->standard_id = $validatedData['standard_id'];
-                $exam->stream_id = (!empty($validatedData['stream_id'])) ? $validatedData['stream_id'] : '';
-                $exam->subject_id = $validatedData['subject_id'];
-                $exam->save();
-                if (!empty($exam->id)) {
-                    return response()->json([
-                        'status' => 200,
-                        'message' => 'Successfully Create Exam.',
-                    ], 200, [], JSON_NUMERIC_CHECK);
+                $exam_data = Exam_Model::where('standard_id', $validatedData['standard_id'])
+                    ->where('exam_date', Carbon::createFromFormat('d-m-Y', $validatedData['exam_date']))
+                    ->where('start_time', $validatedData['start_time'])
+                    ->where('end_time', $validatedData['end_time'])
+                    ->get()->toarray();
+                echo "<pre>";
+                print_r($exam_data);
+                exit;
+                if (empty($exam_data)) {
+                    $exam = new Exam_Model;
+                    $exam->exam_title = $validatedData['exam_title'];
+                    $exam->total_mark = $validatedData['total_mark'];
+                    $exam->exam_type = $validatedData['exam_type'];
+                    $exam->exam_date = Carbon::createFromFormat('d-m-Y', $validatedData['exam_date']);
+                    $exam->start_time = $validatedData['start_time'];
+                    $exam->end_time = $validatedData['end_time'];
+                    $exam->institute_for_id = $validatedData['institute_for_id'];
+                    $exam->board_id = $validatedData['board_id'];
+                    $exam->medium_id = $validatedData['medium_id'];
+                    $exam->class_id = $validatedData['class_id'];
+                    $exam->standard_id = $validatedData['standard_id'];
+                    $exam->stream_id = (!empty($validatedData['stream_id'])) ? $validatedData['stream_id'] : '';
+                    $exam->subject_id = $validatedData['subject_id'];
+                    $exam->save();
+                    if (!empty($exam->id)) {
+                        return response()->json([
+                            'status' => 200,
+                            'message' => 'Successfully Create Exam.',
+                        ], 200, [], JSON_NUMERIC_CHECK);
+                    } else {
+                        return response()->json([
+                            'status' => 400,
+                            'message' => 'Not inserted.',
+                        ]);
+                    }
                 } else {
                     return response()->json([
                         'status' => 400,
-                        'message' => 'Not inserted.',
+                        'message' => 'Already Exist Exam!.',
                     ]);
                 }
             } catch (ValidationException $e) {
