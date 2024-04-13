8cab5364fda6db843a3b8f66661740652e280142
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-04-13 10:34:16 +0530

    Videotimelimitcontroller

--- app/Http/Controllers/Videotimelimitcontroller.php
+++ app/Http/Controllers/Videotimelimitcontroller.php
@@ -8,12 +8,21 @@ use Illuminate\Http\Request;
 class Videotimelimitcontroller extends Controller
 {
     public function list(Request $request){
-        $vidtimQY = Video_time_limit_model::get();
+        $vidtimQY = Video_time_limit_model::paginate();
         
         return view('topic/video_time_limit',compact('vidtimQY'));
     }
 
-    public function add(Request $request){
+    public function save(Request $request){
+        $request->validate([
+            'time' => 'required',
+        ]);
 
+        Video_time_limit_model::create([
+            'time'=>$request->input('time'),
+        ]);
+    
+        return redirect()->route('videolimit.list')->with('success', 'Created Successfully');
+    
     }
 }
