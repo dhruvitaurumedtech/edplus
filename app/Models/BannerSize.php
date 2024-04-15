b118f2c12fe8b45f1253a6fe9d8d3bd1a06c8031
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-04-15 16:32:32 +0530

    C

--- app/Models/BannerSize.php
+++ app/Models/BannerSize.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BannerSize extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = ['size', 'width', 'height'];
 }
--- database/migrations/2024_04_15_063703_create_banner_sizes_table.php
+++ database/migrations/2024_04_15_063703_create_banner_sizes_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->integer('width');
             $table->integer('height');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
