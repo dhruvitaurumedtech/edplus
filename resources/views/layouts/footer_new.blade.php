67ad7797e4d94983e8fb09acb95552ad08e69284
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-04-15 10:56:54 +0530

    c

--- resources/views/layouts/footer_new.blade.php
+++ resources/views/layouts/footer_new.blade.php
@@ -1,6 +1,18 @@
+<?php
+$currentMonth = date('n'); // Get the current month (numeric, without leading zeros)
+$currentYear = date('Y');   // Get the current year
+
+// Determine the academic year
+if ($currentMonth >= 9) { // If the current month is September or later
+    $academicYear = $currentYear . '-' . ($currentYear + 1);
+} else { // If the current month is before September
+    $academicYear = ($currentYear - 1) . '-' . $currentYear;
+}
+
+?>
 <footer class="footer">
     <div class="footer-text">
-        <p>All rights reserved Copyright &#169; 2023-24 Design &amp; Developed By <a href="javascript:void(0)">Aurum Edtech</a> </p>
+        <p>All rights reserved Copyright &#169; <?php echo $academicYear ?> Design &amp; Developed By <a href="javascript:void(0)">Aurum Edtech</a> </p>
     </div>
 </footer>
 
