855ee1ea4b1ae42edc14e09a6db97e697b702014
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-04-15 19:10:17 +0530

    StudentController

--- app/Http/Controllers/API/student/StudentController.php
+++ app/Http/Controllers/API/student/StudentController.php
@@ -133,15 +133,17 @@ class StudentController extends Controller
                 }
 
                 //join with
+                
                 $joininstitute = Institute_detail::where('status', 'active')
                 ->whereIn('id', function ($query) use ($user_id) {
                     $query->select('institute_id')
                         ->where('student_id', $user_id)
                         ->where('status', '=', '1')
-                        ->where('end_academic_year', '>=', today())
+                        ->where('end_academic_year', '>=', now())
                         ->from('students_details')
                         ->whereNull('deleted_at');
-                })->paginate($perPage);
+                })
+                ->where('end_academic_year', '>=', now())->paginate($perPage);
                 $join_with = [];
                 foreach ($joininstitute as $value) {
                     $join_with[] = array(
