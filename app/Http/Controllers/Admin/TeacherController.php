953455d75500cea222d44528c9c9466f746f9a09
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-04-17 20:45:58 +0530

    c

--- app/Http/Controllers/Admin/TeacherController.php
+++ app/Http/Controllers/Admin/TeacherController.php
@@ -284,17 +284,20 @@ class TeacherController extends Controller
             $teacher_id = $request->input('teacher_id');
             $existingUser = User::where('token', $token)->where('id', $teacher_id)->first();
             if ($existingUser) {
-                $batch_list = Batches_model::where('institute_id', $request->institute_id)->where('user_id', $request->teacher_id)
-                    ->select('*')->get()->toarray();
 
-                foreach ($batch_list as $values_batch) {
-                    Batch_assign_teacher_model::create([
-                        'teacher_id' => $request->teacher_id,
-                        'batch_id' => $values_batch['id'],
-                    ]);
-                }
                 $subject = Subject_model::whereIn('id', explode(',', $request->subject_id))->get();
+
                 foreach ($subject as $value) {
+                    $batch_list = Batches_model::whereRaw("FIND_IN_SET($value->id, subjects)")
+                        ->select('*')->get()->toarray();
+
+                    foreach ($batch_list as $values_batch) {
+                        Batch_assign_teacher_model::create([
+                            'teacher_id' => $request->teacher_id,
+                            'batch_id' => $values_batch['id'],
+                        ]);
+                    }
+
                     $base_table_response = Base_table::where('id', $value->base_table_id)->get()->toarray();
                     foreach ($base_table_response as $value2) {
                         Teacher_model::create([
