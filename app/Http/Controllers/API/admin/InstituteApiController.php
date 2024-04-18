e2cab7b51b20e19e966141dd4da0d53885ebbc8b
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-04-18 11:54:31 +0530

    InstituteApiController

--- app/Http/Controllers/API/admin/InstituteApiController.php
+++ app/Http/Controllers/API/admin/InstituteApiController.php
@@ -2136,7 +2136,7 @@ class InstituteApiController extends Controller
                             'name' => $rolDT->role_name
                         );
                     }
-
+                       print_r('texst');exit; 
                     $announcementDT[] = array(
                         'id' => $anoouncmnt->id,
                         'date' => $anoouncmnt->created_at,
