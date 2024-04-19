97235b7ef5afaa33ca4f5cc45598bfdf5dd3228f
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-04-19 12:03:26 +0530

    c

--- app/Http/Controllers/Admin/TeacherController.php
+++ app/Http/Controllers/Admin/TeacherController.php
@@ -146,9 +146,7 @@ class TeacherController extends Controller
                         'logo' => asset($value->logo),
                     );
                 }
-                $query = Common_announcement::whereRaw("FIND_IN_SET($value->teacher_id, teacher_id)")->toSql();
-                print_r($query);
-                $announcement = Common_announcement::whereRaw("FIND_IN_SET($value->teacher_id, teacher_id)")
+                $announcement = Common_announcement::whereRaw("FIND_IN_SET($request->teacher_id, teacher_id)")
                     ->select('*')->get()->toarray();
                 echo "<pre>";
                 print_r($announcement);
