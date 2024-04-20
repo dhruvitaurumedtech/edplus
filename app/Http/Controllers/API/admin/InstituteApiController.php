383693ce99087ce588f4b637ac5a82359cd9ca31
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-04-20 16:53:46 +0530

    InstituteApiController
    new api /Subject-List

--- app/Http/Controllers/API/admin/InstituteApiController.php
+++ app/Http/Controllers/API/admin/InstituteApiController.php
@@ -2890,6 +2890,64 @@ class InstituteApiController extends Controller
         }
     }
 
+    public function subjectList(Request $request){
+        
+            $validator = \Validator::make($request->all(), [
+                'user_id' => 'required',
+                'institute_id' => 'required',
+                'board_id' => 'required',
+                'medium_id' => 'required',
+                'standard_id' => 'required',
+            ]);
+            if ($validator->fails()) {
+                $errorMessages = array_values($validator->errors()->all());
+                return response()->json([
+                    'success' => 400,
+                    'message' => 'Validation error',
+                    'errors' => $errorMessages,
+                ], 400);
+            }
+            $token = $request->header('Authorization');
+            if (strpos($token, 'Bearer ') === 0) {
+                $token = substr($token, 7);
+            }
+            $existingUser = User::where('token', $token)->where('id', $request->user_id)->first();
+            if ($existingUser) {
+                try {
+                    $subjctlist = Subject_sub::join('subject','subject.id','=','subject_sub.subject_id')
+                        ->join('base_table','base_table.id','=','subject.base_table_id')
+                        ->where('subject_sub.user_id', $request->user_id)
+                        ->where('subject_sub.institute_id', $request->institute_id)
+                        ->where('base_table.board_id', $request->board_id)
+                        ->where('base_table.standard_id', $request->standard_id)->get()->toarray();
+                    $batch_response = [];
+                    foreach ($subjctlist as $svalue) {
+                        $batch_response[] = [
+                            'id' => $svalue['id'],
+                            'name' => $svalue['name']
+                        ];
+                    }
+                    return response()->json([
+                        'status' => '200',
+                        'message' => 'Data Fetch Successfully',
+                        'data' => $batch_response
+                    ]);
+                } catch (\Exception $e) {
+                    return response()->json([
+                        'success' => 500,
+                        'message' => 'Server Error',
+                        'error' => $e->getMessage(),
+                    ], 500);
+                }
+            } else {
+                return response()->json([
+                    'status' => 400,
+                    'message' => 'Invalid token.',
+                ]);
+            }
+        
+    }
+
     public function edit_subject(Request $request)
     {
 
