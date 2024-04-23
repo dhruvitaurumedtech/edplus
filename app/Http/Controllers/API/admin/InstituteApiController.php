e7ac51cff2502296b3f2f32793f5cc1ff677fd9a
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-04-23 14:00:21 +0530

    InstituteApiController

--- app/Http/Controllers/API/admin/InstituteApiController.php
+++ app/Http/Controllers/API/admin/InstituteApiController.php
@@ -1094,7 +1094,7 @@ class InstituteApiController extends Controller
                     $subject_array[] = [
                         'id' => $subject_value->id,
                         'subject_value' => $subject_value->name,
-                        'image' => $subject_value->image,
+                        'image' =>asset($subject_value->image) ,
                     ];
                 }
 
