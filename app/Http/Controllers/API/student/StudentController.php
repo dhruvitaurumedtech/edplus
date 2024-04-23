8889bb096232ada2cd707b1917549c10d04e74eb
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-04-23 17:46:17 +0530

    StudentController

--- app/Http/Controllers/API/student/StudentController.php
+++ app/Http/Controllers/API/student/StudentController.php
@@ -140,19 +140,20 @@ class StudentController extends Controller
                 //join with
 
                 $joininstitute = Institute_detail::where('status', 'active')
-    ->whereIn('id', function ($query) use ($user_id) {
-        $query->select('institute_id')
-            ->where('student_id', $user_id)
-            ->where('status', '=', '1')
-            ->from('students_details')
-            ->whereNull('deleted_at');
-    })
-    ->where('end_academic_year', '>=', now())
-    ->toSql();
+                ->whereIn('id', function ($query) use ($user_id) {
+                    $query->select('institute_id')
+                        ->where('student_id', $user_id)
+                        ->where('status', '=', '1')
+                        ->from('students_details')
+                        ->whereNull('deleted_at');
+                })
+                ->where('end_academic_year', '>=', now())
+                ->toSql();
 
-print_r($joininstitute);
-exit;
+                print_r($joininstitute);
+                exit;
 
+                    
                     echo $joininstitute->toSql();exit;
                     $join_with = [];
                 
