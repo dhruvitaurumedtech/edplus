dd57bb4aa5b8f12d3547b667c0d7cf11e2d630d3
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-04-25 18:51:33 +0530

    StudentController

--- app/Http/Controllers/API/student/StudentController.php
+++ app/Http/Controllers/API/student/StudentController.php
@@ -949,7 +949,7 @@ class StudentController extends Controller
                     // $batch_response = [
                     //     'batch_list' => $batch_response,
                     // ];
-                    $response = array($batch_response,$category);
+                    $response = array('batch_list'=>$batch_response,$category);
                     
                 } else {
                     $response = $category; // Assign $category directly to $response
