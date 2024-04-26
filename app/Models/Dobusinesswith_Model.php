d50acb18bc943c75687b4e38097f4581a718093b
keval123@K <kevalpatel@sarvopari.app>
2024-04-26 14:27:18 +0530

    video Assign and Categorie List Api Optimize

--- app/Models/Dobusinesswith_Model.php
+++ app/Models/Dobusinesswith_Model.php
@@ -5,12 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use App\Models\VideoCategory;
 class Dobusinesswith_Model extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory, SoftDeletes;
     protected $table = 'do_business_with';
     protected $fillable = [
-        'name','category_id', 'status', 'created_by', 'updated_by','deleted_at'
+        'name', 'category_id', 'status', 'created_by', 'updated_by', 'deleted_at'
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(VideoCategory::class, 'category_id', 'id');
+    }
 }
