d08ef541bbfa0b101dc17f1fc9177e672fb07d52
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-04-29 15:40:16 +0530

    base class

--- app/Http/Controllers/API/admin/BasetableControllerAPI.php
+++ app/Http/Controllers/API/admin/BasetableControllerAPI.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\board;
+use App\Models\Class_model;
 use App\Models\Institute_for_model;
 use App\Models\Medium_model;
 use App\Traits\ApiTrait;
@@ -74,8 +75,8 @@ class BasetableControllerAPI extends Controller
                 $institute_for_ids = explode(',', $request->institute_for_id);
                 $board_ids = explode(',', $request->board_id);
                 $base_medium = Medium_model::join('base_table', 'base_table.medium', '=', 'medium.id')
-                    ->whereIN('base_table.board',$institute_for_ids)
-                    ->whereIN('base_table.medium',$board_ids)
+                    ->whereIN('base_table.institute_for',$institute_for_ids)
+                    ->whereIN('base_table.board',$board_ids)
                     ->select('medium.id', 'medium.name', 'medium.icon')
                     ->distinct()
                     ->get();
@@ -83,6 +84,43 @@ class BasetableControllerAPI extends Controller
                 foreach ($base_medium as $basemedium) {
                     $data[] = array('id' => $basemedium->id, 'name' => $basemedium->name,
                      'icon' => url($basemedium->icon));
+                }
+                
+            return $this->response($data, "Fetch Data Successfully");
+        } catch (Exeption $e) {
+            return $this->response($e, "Something want Wrong!!", false, 400);
+        }
+    }
+
+
+    public function class(Request $request){
+
+        $validator = \Validator::make($request->all(), [
+            'institute_for_id' => 'required',
+            'board_id' => 'required',
+            'medium_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->response([], $validator->errors()->first(), false, 400);
+        }
+
+        try {
+                $institute_for_ids = explode(',', $request->institute_for_id);
+                $board_ids = explode(',', $request->board_id);
+                $medium_ids = explode(',', $request->medium_id);
+
+                $base_class = Class_model::join('base_table', 'base_table.institute_for_class', '=', 'class.id')
+                    ->whereIN('base_table.institute_for',$institute_for_ids)
+                    ->whereIN('base_table.board',$board_ids)
+                    ->whereIN('base_table.medium',$medium_ids)
+                    ->select('class.id', 'class.name', 'class.icon')
+                    ->distinct()
+                    ->get();
+                $data = [];
+                foreach ($base_class as $baseclass) {
+                    $data[] = array('id' => $baseclass->id, 'name' => $baseclass->name,
+                     'icon' => url($baseclass->icon));
                 }
                 
             return $this->response($data, "Fetch Data Successfully");
--- routes/api.php
+++ routes/api.php
@@ -52,6 +52,7 @@ Route::middleware(['auth:api'])->group(function () {
     Route::post('/institute/base-institute-for', [BasetableControllerAPI::class, 'institute_for']);
     Route::post('/institute/base-board', [BasetableControllerAPI::class, 'board']);
     Route::post('/institute/base-medium', [BasetableControllerAPI::class, 'medium']);
+    Route::post('/institute/base-class', [BasetableControllerAPI::class, 'class']);
 });
 Route::post('/institute/get-request-list', [InstituteApiController::class, 'get_request_list'])->name('request_list.get');
 
