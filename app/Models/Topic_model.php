a1ccb02da25ce9eaa3c48fcef46ba210ec4a4411
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-05-09 15:24:30 +0530

    fetch_teacher_detail,video upload,assign video

--- app/Models/Topic_model.php
+++ app/Models/Topic_model.php
@@ -11,6 +11,5 @@ class Topic_model extends Model
     use HasFactory, SoftDeletes;
     protected $table = 'topic';
     protected $fillable = [
-        'institute_id', 'user_id', 'base_table_id', 'standard_id', 'subject_id', 'chapter_id', 'video_category_id', 'topic_no', 'topic_name', 'topic_description', 'topic_video', 'status', 'created_at', 'updated_at', 'deleted_at'
-    ];
+        'institute_id', 'user_id', 'base_table_id', 'standard_id', 'subject_id', 'chapter_id', 'video_category_id', 'topic_no', 'topic_name', 'topic_description', 'topic_video', 'status', 'created_by', 'updated_by', 'created_at', 'updated_at' ];
 }
