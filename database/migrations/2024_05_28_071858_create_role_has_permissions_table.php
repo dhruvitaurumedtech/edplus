8415aa8a64a15a694c06d56d8a2093bda822c207
keval123@K <kevalpatel@sarvopari.app>
2024-05-28 17:09:20 +0530

    acl database added

---
+++ database/migrations/2024_05_28_071858_create_role_has_permissions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('role_has_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->unsignedBigInteger('feature_id');
+            $table->foreign('feature_id')->references('id')->on('features');
+            $table->unsignedBigInteger('action_id');
+            $table->foreign('action_id')->references('id')->on('actions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('role_has_permissions');
+    }
+};
