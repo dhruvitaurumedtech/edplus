ca296adcd671974d48f5bfff14bbb2456b280584
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-06-11 14:41:20 +0530

    forgot-password

--- resources/views/emails/forgot.blade.php
+++ resources/views/emails/forgot.blade.php
@@ -1,4 +1,10 @@
 <h1>Forget Password Email</h1>
    
-You can reset password from bellow link:
-<a href="{{ route('reset.password.get', $token) }}">Reset Password</a>
+Dear {{$name}},
+We received a request to reset your password for your account associated with this email address.
+To reset your password, please use the following 6-digit verification code:
+    {{$token}}
+
+Please enter this code in the verification field on the password reset page.
+
+If you did not request a password reset, please ignore this email or contact our support team.
