1842537b9e7f85e78ba6ccda995a11262c76757d
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-06-11 14:48:09 +0530

    mail formate

--- resources/views/emails/forgot.blade.php
+++ resources/views/emails/forgot.blade.php
@@ -1,7 +1,6 @@
 <h1>Forget Password Email</h1>
    
 <p>Dear {{$name}},</p>
-<br>
 <p>
 We received a request to reset your password for your account associated with this email address.
 To reset your password, please use the following 6-digit verification code:
@@ -11,7 +10,6 @@ To reset your password, please use the following 6-digit verification code:
  <br>   
 
 <p>Please enter this code in the verification field on the password reset page.</p>
-<br>
 <p>If you did not request a password reset, please ignore this email or contact our support team.</p>
 <br>
 <br>
