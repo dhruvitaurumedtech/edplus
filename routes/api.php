9acae867ea82fdc95ccc4fe998d6dac0f2beaa59
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-06-19 14:01:57 +0530

    c

--- routes/api.php
+++ routes/api.php
@@ -200,14 +200,15 @@ Route::middleware(['auth:api', 'apilogs'])->group(function () {
     Route::post('/institute/fees-collection', [FeesController::class, 'fees_collection'])->middleware('check.permission:7,1');
 
     Route::post('/institute/add-classroom', [InstituteApiController::class, 'Add_classRoom']);
-});
-Route::post('/institute/create-general-timetable', [General_timetableController::class, 'create_general_timetable']);
-Route::post('/institute/display-general-timetable', [General_timetableController::class, 'display_general_timetable']);
-Route::post('/institute/edit-general-timetable', [General_timetableController::class, 'edit_general_timetable']);
-Route::post('/institute/delete-general-timetable', [General_timetableController::class, 'delete_general_timetable']);
-Route::post('/institute/institute-day-filter-general-timetable', [General_timetableController::class, 'institute_day_filter_general_timetable']);
-Route::post('/institute/batch-standard-filter-general-timetable', [General_timetableController::class, 'batch_standard_filter_general_timetable']);
+    Route::post('/institute/create-general-timetable', [General_timetableController::class, 'create_general_timetable']);
+    Route::post('/institute/display-general-timetable', [General_timetableController::class, 'display_general_timetable']);
+    Route::post('/institute/edit-general-timetable', [General_timetableController::class, 'edit_general_timetable']);
+    Route::post('/institute/delete-general-timetable', [General_timetableController::class, 'delete_general_timetable']);
+    Route::post('/institute/institute-day-filter-general-timetable', [General_timetableController::class, 'institute_day_filter_general_timetable']);
+    Route::post('/institute/batch-standard-filter-general-timetable', [General_timetableController::class, 'batch_standard_filter_general_timetable']);
     
+
+});
 
 
 
