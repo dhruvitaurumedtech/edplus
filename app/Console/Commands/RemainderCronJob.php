865ec2c60371515703bdbf07592b6897f968e036
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-06-24 09:44:35 +0530

    c

--- app/Console/Commands/RemainderCronJob.php
+++ app/Console/Commands/RemainderCronJob.php
@@ -32,6 +32,7 @@ class RemainderCronJob extends Command
         date_default_timezone_set('Asia/Kolkata');
         $data=Remainder_model::get();
         foreach($data as $value){
+            if($value->type_field==1){
             $givenDateString = $value->date;
             $timeString = $value->time;
             $dateTime = Carbon::createFromFormat('H:i:s', $timeString);
@@ -94,9 +95,68 @@ class RemainderCronJob extends Command
                     curl_close($ch);
                 }
             } 
-        }
+         }
+         if($value->type_field==2){
+            $givenDateString = $value->date;
+            $currentDateString = Carbon::now()->format('Y-m-d');
 
- 
-        \Log::info('SampleCronJob is running successfully.');
+            if ($givenDateString === $currentDateString) {
+                
+                $serverKey = env('SERVER_KEY');
+
+                $url = "https://fcm.googleapis.com/fcm/send";
+                if(!empty($value->student_id)){
+                    $users = User::where('id', $value->student_id)->pluck('device_key');
+                }
+                if(!empty($value->role_type_id)){
+                    $users = User::where('role_type', $value->role_type_id)->pluck('device_key')->toArray();
+                }
+                // \Log::info($users);
+                $notificationTitle = $value->title;
+                $notificationBody = $value->message;
+
+                $data = [
+                    'registration_ids' => $users,
+                    'notification' => [
+                        'title' => $notificationTitle,
+                        'body' => $notificationBody,
+                        'click_action' => 'FLUTTER_NOTIFICATION_CLICK',
+                    ],
+                ];
+
+                if (!empty($users)) {
+                    $json = json_encode($data);
+
+                    $headers = [
+                        'Content-Type: application/json',
+                        'Authorization: key=' . $serverKey
+                    ];
+
+                    $ch = curl_init();
+                    curl_setopt_array($ch, [
+                        CURLOPT_URL => $url,
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'POST',
+                        CURLOPT_POSTFIELDS => $json,
+                        CURLOPT_HTTPHEADER => $headers,
+                    ]);
+
+                    $result = curl_exec($ch);
+
+                    if ($result === FALSE) {
+                    }
+
+                    curl_close($ch);
+                }
+            } 
+         }
+         
+
+       }
     }
 }
