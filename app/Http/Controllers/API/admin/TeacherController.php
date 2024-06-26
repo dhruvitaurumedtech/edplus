8982a89e7919a29c81f49e53b8c26dcc9a414f13
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-06-26 17:32:29 +0530

    c

--- app/Http/Controllers/API/admin/TeacherController.php
+++ app/Http/Controllers/API/admin/TeacherController.php
@@ -587,6 +587,7 @@ class TeacherController extends Controller
                 ->where('teacher_detail.teacher_id', $teacher_id)
                 ->where('teacher_detail.institute_id', $institute_id)
                 //->whereNull('teacher_detail.deleted_at')
+                ->group_By('standard.id')
                 ->select(
                     'board.name as board_name',
                     'standard.id as standard_id',
@@ -631,7 +632,7 @@ class TeacherController extends Controller
             return $this->response([], $validator->errors()->first(), false, 400);
         }
         try {
-            $value = Teacher_model::join('batches', 'batches.id', '=', 'teacher_detail.batch_id')
+            $teacher_data = Teacher_model::join('batches', 'batches.id', '=', 'teacher_detail.batch_id')
                 ->Join('board', 'board.id', '=', 'teacher_detail.board_id')
                 ->Join('medium', 'medium.id', '=', 'teacher_detail.medium_id')
                 ->Join('standard', 'standard.id', '=', 'teacher_detail.standard_id')
@@ -648,11 +649,11 @@ class TeacherController extends Controller
                     'batches.batch_name',
                     'batches.subjects'
                 )
-                ->first();
+                ->get()->toarray();
                 
             $teacher_response = [];
 
-            // foreach ($teacher_data as $value) {
+            foreach ($teacher_data as $value) {
                 $subject_data = Subject_model::whereIn('id', explode(',', $value->subjects))->get();
                 $subject_response = [];
                 foreach ($subject_data as $subject_value) {
@@ -674,7 +675,7 @@ class TeacherController extends Controller
                     'batch' => $value->batch_name,
                     'subject_list' => $subject_response,
                 ];
-            // }
+            }
             return $this->response($teacher_response, "Data Fetch Successfully");
         } catch (Exception $e) {
             return $this->response($e, "Invalid token.", false, 400);
