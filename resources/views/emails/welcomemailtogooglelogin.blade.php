9af16a4fca9afc6a34c66e9d776b1d1ed6cb50a5
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-07-19 14:07:17 +0530

    general timetable | welcome mail of google login

---
+++ resources/views/emails/welcomemailtogooglelogin.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Reset Your Password</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            color: #333;
+        }
+        h1 {
+            color: #4CAF50;
+        }
+        p {
+            margin: 0.5em 0;
+        }
+        .code {
+            font-size: 1.2em;
+            font-weight: bold;
+            color: #FF5733;
+        }
+    </style>
+</head>
+<body>
+Dear {{$name}},
+
+<p>Welcome to EdWide.</p>
+<p>We are thrilled to have you join our community. By registering with us, you’ve taken the first step towards  on below.
+Here’s how to get started:</p>
+
+<p>1.Explore: Dive into the features and tools that EdWide offers.</p>
+
+<p>2.Customize: Set up your profile and preferences to tailor your experience.</p>
+
+<p>3.Engage: Connect with our community, access resources, and stay updated.</p>
+
+<p>Thank you for choosing EdWide. We’re excited to support you on your journey.</p>
+
+<p>Best Regards,</p>
+<p>The EdWide Team</p>    
+</body>
+</html>
+
+
