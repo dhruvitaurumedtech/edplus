ce9cdf7ea85c2e4833ec25d0e2c97a32f6045f5f
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-07-22 19:07:36 +0530

    c

--- app/Http/Controllers/API/admin/InstituteApiController.php
+++ app/Http/Controllers/API/admin/InstituteApiController.php
@@ -3231,7 +3231,7 @@ class InstituteApiController extends Controller
             $batch_id = $request->batch_id;
             $studentdtls = Student_detail::where('student_id', $student_id)
                 ->where('institute_id', $institute_id)->first();
-                if($studentdtls->reject_count==2){
+                if($studentdtls->reject_count==3){
                     return $this->response([], "Two time reject request account is blocked!!", false, 400);
                 }
             $insdelQY = Standard_sub::where('board_id', $request->board_id)
@@ -6183,7 +6183,7 @@ class InstituteApiController extends Controller
             $teacher_detail = json_decode($request->teacher_detail, true);
             foreach($teacher_detail as $teacherDT){
                 $teacherDetail = Teacher_model::where('id', $teacherDT['teacher_detail_id'])->first();
-                if($teacherDT['reject_count']==2){
+                if($teacherDT['reject_count']==3){
                     return $this->response([], "Two time reject account blocked!!", false, 400);
                 }
                 if ($teacherDetail) {
