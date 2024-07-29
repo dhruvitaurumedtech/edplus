c850c4dd855f60ab776c2f0b6d44da8c11a0a20b
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-07-29 16:27:14 +0530

    c

--- app/Http/Controllers/API/admin/InstituteApiController.php
+++ app/Http/Controllers/API/admin/InstituteApiController.php
@@ -6036,7 +6036,7 @@ class InstituteApiController extends Controller
     public function do_business_with()
     {
         try {
-            $data = Dobusinesswith_Model::all();
+            $data = Dobusinesswith_Model::where('status','active')->all();
             if (!empty($data)) {
                 foreach ($data as $value) {
                     $response[] = ['id' => $value->id, 'name' => $value->name];
