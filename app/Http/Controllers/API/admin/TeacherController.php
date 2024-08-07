d959c379b03ef8487aa8044a8f4e946e8dae88b5
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-08-07 16:06:42 +0530

    c

--- app/Http/Controllers/API/admin/TeacherController.php
+++ app/Http/Controllers/API/admin/TeacherController.php
@@ -356,10 +356,11 @@ class TeacherController extends Controller
             $url = "https://fcm.googleapis.com/fcm/send";
             $inst_owner_id = Institute_detail::where('id', $request->institute_id)->first();
             $users = User::where('id', $inst_owner_id->user_id)->pluck('device_key');
-
+           
             $notificationTitle = "Teacher Join Request";
-            $notificationBody = $request->firstname . " Requestd To Join Your Institute";
-
+            $notificationBody = $request->firstname." Requestd To Join Your Institute";
+            print_r($inst_owner_id->user_id);
+            print_r($notificationBody);exit;
             $data = [
                 'registration_ids' => $users,
                 'notification' => [
