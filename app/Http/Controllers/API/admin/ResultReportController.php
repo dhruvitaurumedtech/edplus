7bb77de8c17fb08a3e8632bccd5cc736f5e51544
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-09-09 17:53:37 +0530

    result report in changes

--- app/Http/Controllers/API/admin/ResultReportController.php
+++ app/Http/Controllers/API/admin/ResultReportController.php
@@ -7,6 +7,8 @@ use App\Models\Student_detail;
 use App\Traits\ApiTrait;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use PDF;
 use Illuminate\Support\Facades\Validator;
 
 class ResultReportController extends Controller
@@ -29,8 +31,16 @@ class ResultReportController extends Controller
             ->leftjoin('marks','marks.student_id','=','students_details.student_id')
             ->leftjoin('exam','exam.id','=','marks.exam_id')
             ->leftjoin('subject', 'subject.id', '=', 'exam.subject_id')
-            ->select('users.*','board.name as board_name','standard.name as standard_name','medium.name as medium_name',
-                     'batches.batch_name','class.name as class_name')
+            ->select('users.*', 
+            'board.name as board_name', 
+            'standard.name as standard_name', 
+            'medium.name as medium_name',
+            'batches.batch_name', 
+            'class.name as class_name', 
+            'marks.mark', 
+            'exam.total_mark', 
+            'exam.exam_title as exam_name', 
+            'exam.exam_date as exam_date','subject.name as subject_name')
             ->where('students_details.institute_id',$request->institute_id)
             ->when(!empty($request->subject_id) ,function ($query) use ($request){
                 return $query->where('exam.subject_id', $request->subject_id);
@@ -47,6 +57,9 @@ class ResultReportController extends Controller
             ->when(!empty($request->medium_id) ,function ($query) use ($request){
                 return $query->where('students_details.medium_id', $request->medium_id);
             })
+            ->when(!empty($request->standard_id) ,function ($query) use ($request){
+                return $query->where('students_details.standard_id', $request->standard_id);
+            })
             ->when(!empty($request->exam_name) ,function ($query) use ($request){
                 return $query->where('exam.exam_title', $request->exam_name);
             })
@@ -56,11 +69,30 @@ class ResultReportController extends Controller
             ->when(!empty($request->student_id) ,function ($query) use ($request){
                 return $query->where('students_details.student_id', $request->student_id);
             })
-            ->get();
-            print_r($data);exit;
-
-          } catch (Exception $e) {
-            return $this->response([], "Something went wrong!.", false, 400);
-        }
+            ->get()->toarray();
+            $data = ['result_report'=>$data,'request_data'=>$request]; 
+           $pdf = PDF::loadView('pdf.result_report', ['data' => $data])->setPaper('A4', 'portrait')->setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true]);;
+            
+            $folderPath = public_path('pdfs');
+    
+            if (!File::exists($folderPath)) {
+                File::makeDirectory($folderPath, 0755, true);
+            }
+            $baseFileName = 'result_report.pdf';
+            $pdfPath = $folderPath . '/' . $baseFileName;
+    
+            $counter = 1;
+            while (File::exists($pdfPath)) {
+                $pdfPath = $folderPath . '/result_report' . $counter . '.pdf'; 
+                $counter++;
+            }
+            
+            file_put_contents($pdfPath, $pdf->output());
+            $pdfUrl = asset('pdfs/' . basename($pdfPath));
+            return $this->response($pdfUrl);
+            } catch (Exception $e) {
+                return $this->response([], "Something want wrong!.", false, 400);
+            }
+    
     }
 }
