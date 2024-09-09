e2948242bb939e5bf918e541ab6f2067b216fa8f
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-09-09 18:59:18 +0530

    no data available code

--- resources/views/pdf/result_report.blade.php
+++ resources/views/pdf/result_report.blade.php
@@ -66,6 +66,8 @@
 
     <h2>Result List</h2>
     <hr>
+    @if(!empty($data['result_report']))
+
     @if(!empty($data['request_data']['board_id']))
     <p><b>Board_name:</b> {{ $data['result_report'][0]['board_name'] }}</p>
     @endif
@@ -85,6 +87,8 @@
     @if(!empty($data['request_data']['standard_id']))
         <p><b>Standard_name:</b> {{ $data['result_report'][0]['standard_name'] }}</p>
     @endif
+    @endif
+
 
    
 
