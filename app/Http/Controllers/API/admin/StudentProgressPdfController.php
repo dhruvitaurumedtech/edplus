2c34c0549116ccec1e8a0107df7c980d9a483b9d
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-09-19 18:10:21 +0530

    student progress report work on

--- app/Http/Controllers/API/admin/StudentProgressPdfController.php
+++ app/Http/Controllers/API/admin/StudentProgressPdfController.php
@@ -120,13 +120,157 @@ class StudentProgressPdfController extends Controller
                             $batch_result=[];
                               foreach($batch_response as $batch_value){
                                 $batch_id = !empty($request->batch_id) ? $request->batch_id : $batch_value['batch_id'];
-                          
+                                
+                                $subject_get=Student_detail::leftjoin('standard', 'standard.id', '=', 'students_details.standard_id')
+                                ->when(!empty($request->institute_id), function ($query) use ($request) {
+                                    return $query->where('students_details.institute_id', $request->institute_id);
+                                  })
+                                  ->when(!empty($board_id), function ($query) use ($board_id) {
+                                    return $query->where('students_details.board_id', $board_id);
+                                    })
+                                  ->when(!empty($medium_id), function ($query) use ($medium_id) {
+                                    return $query->where('students_details.medium_id', $medium_id);
+                                  })
+                                  ->when(!empty($class_id), function ($query) use ($class_id) {
+                                    return $query->where('students_details.class_id', $class_id);
+                                  })
+                                  ->when(!empty($standard_id), function ($query) use ($standard_id) {
+                                    return $query->where('students_details.standard_id', $standard_id);
+                                  }) 
+                                  ->when(!empty($batch_id), function ($query) use ($batch_id) {
+                                    return $query->where('students_details.batch_id', $batch_id);
+                                  }) 
+                                  
+                                ->select('students_details.subject_id')
+                                ->distinct()
+                                ->pluck('students_details.subject_id'); 
+                               
+                                $mergedArray = [];
+                                foreach ($subject_get as $item) {
+                                    $mergedArray = array_merge($mergedArray, explode(',', $item));
+                                }
+                                
+                                $uniqueArray = array_unique($mergedArray);
+                                
+                                $uniqueArray = array_values($uniqueArray);
+                                
+                                $subject_id=!empty($request->subject_id) ? explode(',',$request->subject_id) : $uniqueArray;
+                                         
+                                //         $subject_response = Subject_model::whereIn('id', $subject_all_get)
+                                //         ->select('subject.id as subject_id', 'subject.name as subject_name')
+                                //         ->get()->toarray();  
+                                //         $subject_result= [];
+                                       
+                                //           foreach($subject_response as  $subject_value){
+                                // $subject_id=!empty($request->subject_id) ? $request->subject_id : $subject_value['subject_id'];
+                                
+                                // $subject_id_array = explode(',',$subject_ids);
+                                // print_r($subject_id);
+
+                                         
+                                                $exam_response = Student_detail::leftjoin('marks', 'marks.student_id', '=', 'students_details.student_id')
+                                                            ->leftjoin('exam', 'exam.id', '=', 'marks.exam_id')
+                                                            ->leftJoin('subject', 'subject.id', '=', 'exam.subject_id')
+                                                               
+                                                            ->when(!empty($request->institute_id), function ($query) use ($request) {
+                                                                return $query->where('students_details.institute_id', $request->institute_id);
+                                                            })
+                                                            ->when(!empty($board_id), function ($query) use ($board_id) {
+                                                                return $query->where('students_details.board_id', $board_id);
+                                                            })
+                                                            ->when(!empty($medium_id), function ($query) use ($medium_id) {
+                                                                return $query->where('students_details.medium_id', $medium_id);
+                                                            })
+                                                            ->when(!empty($class_id), function ($query) use ($class_id) {
+                                                                return $query->where('students_details.class_id', $class_id);
+                                                            })
+                                                            ->when(!empty($standard_id), function ($query) use ($standard_id) {
+                                                                return $query->where('students_details.standard_id', $standard_id);
+                                                            })
+                                                            ->when(!empty($batch_id), function ($query) use ($batch_id) {
+                                                                return $query->where('students_details.batch_id', $batch_id);
+                                                            })
+                                                            ->when(!empty($subject_id), function ($query) use ($subject_id) {
+                                                                return $query->whereIn('exam.subject_id', $subject_id);
+                                                            })
+                                                            ->when(!empty($request->exam_name) ,function ($query) use ($request){
+                                                                return $query->where('exam.exam_title', $request->exam_name);
+                                                            })
+                                                            ->when(!empty($request->exam_date) ,function ($query) use ($request){
+                                                                return $query->where('exam.exam_date', $request->exam_date);
+                                                            })
+                                                           ->where('students_details.reject_count', '0')
+                                                            ->whereNull('students_details.deleted_at')
+                                                            ->select('exam.*','subject.name as subject_name') 
+                                                            ->distinct()
+                                                            ->get()
+                                                            ->toArray();
+                                                            $exam_result = [];
+                                                            foreach($exam_response as $exam_value){
+                                                                $exam_id = $exam_value['id'];
+
+                                                                $exam_wise_student_response = Student_detail::leftJoin('marks', 'marks.student_id', '=', 'students_details.student_id')
+                                                                ->leftJoin('exam', 'exam.id', '=', 'marks.exam_id')
+                                                                ->leftJoin('users', 'users.id', '=', 'students_details.student_id')
+                                                                ->when(!empty($request->institute_id), function ($query) use ($request) {
+                                                                    return $query->where('students_details.institute_id', $request->institute_id);
+                                                                })
+                                                                ->when(!empty($board_id), function ($query) use ($board_id) {
+                                                                    return $query->where('students_details.board_id', $board_id);
+                                                                })
+                                                                ->when(!empty($medium_id), function ($query) use ($medium_id) {
+                                                                    return $query->where('students_details.medium_id', $medium_id);
+                                                                })
+                                                                ->when(!empty($class_id), function ($query) use ($class_id) {
+                                                                    return $query->where('students_details.class_id', $class_id);
+                                                                })
+                                                                ->when(!empty($standard_id), function ($query) use ($standard_id) {
+                                                                    return $query->where('students_details.standard_id', $standard_id);
+                                                                })
+                                                                ->when(!empty($batch_id), function ($query) use ($batch_id) {
+                                                                    return $query->where('students_details.batch_id', $batch_id);
+                                                                })
+                                                                ->when(!empty($exam_id), function ($query) use ($exam_id) {
+                                                                    return $query->where('marks.exam_id', $exam_id);
+                                                                })
+                                                                ->where('students_details.reject_count', '0')
+                                                                ->whereNull('students_details.deleted_at')
+                                                                ->select('marks.mark', 'users.firstname', 'users.lastname')
+                                                                ->distinct()
+                                                                ->get()
+                                                                ->toArray();
+                                                            
+                                                             
+                                                                $exam_student_result = [];
+                                                                foreach($exam_wise_student_response as $exam_student_value){
+                                                                    $exam_student_result[] = [
+                                                                        'student_name' => $exam_student_value['firstname'].''.$exam_student_value['lastname'],
+                                                                        'marks'=>$exam_student_value['mark']
+                                                                       
+                                                                    
+                                                                    ];
+                                                                }
+
+                                                                
+                                                                $exam_result[] = [
+                                                                    'exam_name' => $exam_value['exam_title'],
+                                                                    'subject_name'=>$exam_value['subject_name'],
+                                                                    'total_mark' =>$exam_value['total_mark'],
+                                                                    'exam_date' =>$exam_value['exam_date'],
+                                                                    'start_time' =>$exam_value['start_time'],
+                                                                    'end_time' =>$exam_value['end_time'],
+                                                                    'exam_wise_student'=>$exam_student_result,
+
+                                                                
+                                                                ];
+                                                 
+                                    }
                              
-                                    
                                 
                                 $batch_result[] = [
                                     'batch_id' => $batch_value['batch_id'],
                                     'batch_name' => $batch_value['batch_name'],
+                                    'student' => $exam_result,
                                  
                                     
                                 ];
@@ -160,6 +304,7 @@ class StudentProgressPdfController extends Controller
                     'medium' => $medium_result,
                 ];
             }
+            print_r($board_result);exit;
             $data = ['board_result'=>$board_result,'request_data'=>$request];
             $pdf = PDF::loadView('pdf.studentprogressreport', ['data' => $data]);
           
