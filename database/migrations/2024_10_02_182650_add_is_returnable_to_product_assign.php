ce916c81825274d7a1e0b6c15743513dc3e4e0b0
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-10-02 18:28:56 +0530

    new migration for is returnable

--- database/migrations/2024_10_02_182650_add_is_returnable_to_product_assign.php
+++ database/migrations/2024_10_02_182650_add_is_returnable_to_product_assign.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products_assign', function (Blueprint $table) {
-            $table->date('return_date')->nullable()->after('quantity');
+            $table->enum('is_returnable', ['0', '1'])->default('0')->after('return_date');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products_assign', function (Blueprint $table) {
-            $table->dropColumn('return_date');
+            $table->dropColumn('is_returnable');
         });
     }
 };
