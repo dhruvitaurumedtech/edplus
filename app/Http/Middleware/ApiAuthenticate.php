5feafd89fbec82081198de2cca0ab54b4eb697df
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-10-22 17:12:46 +0530

    api resposne time html print

--- app/Http/Middleware/ApiAuthenticate.php
+++ app/Http/Middleware/ApiAuthenticate.php
@@ -9,7 +9,6 @@ class ApiAuthenticate
 {
     public function handle($request, Closure $next, $guard = 'api')
     {
-        // Check if the user is authenticated using the specified guard
         if (Auth::guard($guard)->guest()) {
             return response()->json([
                 'data' => [],
@@ -18,7 +17,6 @@ class ApiAuthenticate
             ], 401);
         }
 
-        // If authenticated, pass the request to the next middleware or controller
         return $next($request);
     }
 }
